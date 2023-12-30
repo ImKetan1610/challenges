4cff347a865f9f3742e66b4b73a85de1ed849a73
ImKetan1610 <ketan.makode@gmail.com>
2023-12-30 12:17:01 +0530

    final commit

--- challenge-accordion/src/App.js
+++ challenge-accordion/src/App.js
@@ -4,7 +4,7 @@ import Accordion from "./Components/Accordion";
 function App() {
   return (
     <div className="App">
-      <h1 style={{ fontSize: "xx-large" }}>
+      <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
         <i>Some Important Question of React</i>
       </h1>
       <Accordion />
